b703997103121c4d0e9a4446ec2295d6f7338c7b
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-07-23 21:21:26 -0500

    chore: Update site URL to Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devatlas.dev',
+  site: 'https://dev-atlascode.vercel.app',
   trailingSlash: 'never',
   integrations: [sitemap()],
   vite: {
